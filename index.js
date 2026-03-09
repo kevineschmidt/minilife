0d8ff65bdd8d3fdad70b3d1ef887a2c8c8fcfbae
Kevin Schmidt <kevin@eyesopen.com>
2026-03-09 08:48:43 -0400

    Updates

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ colors[YELLOW] = 'yellow'
 
 const BORDER_FORCE_FRACTION = .05
 
-const NUM_POINTS = 1000
+const NUM_POINTS = 2000
 const WIDTH= 750
 const HEIGHT= WIDTH
 const FORCE_RANGE = 20
@@ -47,7 +47,7 @@ function generateForceMatrix() {
     for (let i = 0; i < LAST_COLOR; i++) {
         forceMatrix[i] = []
         for (let j = 0; j < LAST_COLOR; j++) {
-            forceMatrix[i][j] = Math.random() * 2 - 1;
+            forceMatrix[i][j] = Math.round(100 * (Math.random() * 2 - 1)) / 100
         }
     }
     const table = document.createElement('table')
@@ -70,16 +70,16 @@ function generateForceMatrix() {
         table.appendChild(tr)
         colors.forEach((color2, idx2) => {
             td = document.createElement('td')
-            td.innerHTML = forceMatrix[idx1][idx2].toFixed(3)
+            const input = document.createElement('input')
+            input.value = forceMatrix[idx1][idx2]
+            input.onchange = (e) => {
+                forceMatrix[idx1][idx2] = parseFloat(e.target.value)
+            }
+            td.appendChild(input)
+
             tr.appendChild(td)
         })
     })
-    /*
-    forceMatrix[BLUE][BLUE] = 2
-     forceMatrix[BLUE][RED] = 1
-    forceMatrix[RED][BLUE] = -1
-    forceMatrix[RED][RED] = 2
-     */
 }
 
 function applyEdgeForces(deltaT) {
@@ -106,26 +106,32 @@ function applyEdgeForces(deltaT) {
     })
 }
 
+const FORCE_RANGE_SQUARED = FORCE_RANGE * FORCE_RANGE
 function applyPointPointForces(deltaT) {
+
     for (let i = 0; i < points.length; i++) {
-        for (let j = i+1; j < points.length; j++) {
-            const p1 = points[i]
+      const p1 = points[i]
+      const [x0, y0] = p1.pos
+      for (let j = i+1; j < points.length; j++) {
             const p2 = points[j]
+
+            const deltaX = p2.pos[0] - x0
+            const deltaY = p2.pos[1] - y0
+
+            const distSquared = deltaX * deltaX  + deltaY * deltaY
+            if (distSquared > FORCE_RANGE_SQUARED) continue
+            if (distSquared === 0) continue
+
             let force1 = forceMatrix[p1.color][p2.color];
             let force2 = forceMatrix[p2.color][p1.color];
+            if (force1 === 0 && force2 === 0) continue
 
-            const deltaX = p2.pos[0] - p1.pos[0]
-            const deltaY = p2.pos[1] - p1.pos[1]
-            if (Math.abs(deltaX) > FORCE_RANGE || Math.abs(deltaY) > FORCE_RANGE) continue
-            const dist = Math.sqrt(deltaX * deltaX  + deltaY * deltaY)
-            if (dist > FORCE_RANGE) continue
-            if (dist === 0) continue
+            const dist = Math.sqrt(distSquared)
             if (dist < MIN_FORCE_RANGE) {
                 force1 = dist - MIN_FORCE_RANGE
                 force2 = dist - MIN_FORCE_RANGE
             }
 
-            if (force1 === 0 && force2 === 0) continue
 
 
             let appliedForce = force1 * dist / FORCE_RANGE
